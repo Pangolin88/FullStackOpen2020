d83a98f40172294df3ca9b9fe4b0253abeb9b92c
Pangolin <huynhviha1703@gmail.com>
2021-01-25 22:39:10 +0700

    update part 2.11 phonebook use axios and effect hook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect} from 'react'
+import axios from 'axios'
 import Persons from "./components/Persons";
 import Filter from "./components/Filter";
-import PersonForm from "./components/PersonForm";
+import PersonForm from "./components/PersonForm"
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewPhone ] = useState('')
   const [ searchName, setSearchName ] = useState('')
 
+  useEffect(() =>{
+      axios
+          .get('http://localhost:3001/persons')
+          .then(reponse => {
+              setPersons(reponse.data)
+          })
+  }, [])
 
   const addPerson = (event) => {
       event.preventDefault()
@@ -46,7 +49,9 @@ const App = () => {
       setSearchName(event.target.value)
   }
 
-  const searchPersons = searchName === '' ? persons : persons.filter(person => person.name.toLocaleLowerCase().includes(searchName.toLocaleLowerCase()))
+  const searchPersons = searchName === ''
+      ? persons
+      : persons.filter(person => person.name.toLocaleLowerCase().includes(searchName.toLocaleLowerCase()))
 
   return (
     <div>

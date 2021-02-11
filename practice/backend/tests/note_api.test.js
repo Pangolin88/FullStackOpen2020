9a4c48cb510ba27c43106d2d85bd3aa1979794c0
Pangolin <huynhviha1703@gmail.com>
2021-02-11 14:08:29 +0700

    part4.b note-app Initializing the database before tests

--- practice/backend/tests/note_api.test.js
+++ practice/backend/tests/note_api.test.js
@@ -4,6 +4,27 @@ const app = require('../app')
 
 const api = supertest(app)
 
+const Note = require('../models/note')
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    date: new Date(),
+    important: false,
+  },
+  {
+    content: 'Browser can execute only Javascript',
+    date: new Date(),
+    important: true,
+  },
+]
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
+
 test('notes are returned as json', async () => {
   await api
     .get('/api/notes')
@@ -13,13 +34,15 @@ test('notes are returned as json', async () => {
 
 test('there are two notes', async () => {
   const response = await api.get('/api/notes')
-
-  expect(response.body).toHaveLength(0)
+  expect(response.body).toHaveLength(initialNotes.length)
 })
 
 test('the first note is about HTTP methods', async () => {
   const response = await api.get('/api/notes')
-  expect(response.body[0].content).toBe('')
+  const contents = response.body.map(r => r.content)
+  expect(contents).toContain(
+    'Browser can execute only Javascript'
+  )
 })
 
 afterAll(() => {

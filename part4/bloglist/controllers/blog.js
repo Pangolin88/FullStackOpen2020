97b149cfe0446dc608a8f24ba9a69b02a0b73f23
Pangolin <huynhviha1703@gmail.com>
2021-02-13 22:53:53 +0700

    part 4.c update bloglist user administration

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -1,18 +1,28 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', {'username': 1})
     response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   if (!request.body.title || !request.body.url)
       return response.status(400).end()
+
+  const user = await User.findById(request.body.userId)
   const blog = new Blog(request.body)
+
+  blog.user = user._id
   if (!blog.likes)
       blog.likes = 0
+
   const addedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(addedBlog._id)
+  user.save()
+
   response.status(201).json(addedBlog)
 })
 

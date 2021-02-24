3c80c3b193261e6f180ce85e430f50af840b4b14
Pangolin <huynhviha1703@gmail.com>
2021-02-24 13:42:39 +0700

    part 6.c complete exercise 6.13

--- part6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -1,31 +1,14 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
-
-const anecdoteReducer = (state = initialState, action) => {
+const anecdoteReducer = (state = [], action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
   switch (action.type) {
     case 'CREATE':
       return [...state, action.data]
+    case 'INIT':
+      return action.data
     case 'VOTE':
       const anecdoteToUpdate = state.find(anecdote => anecdote.id === action.data.id)
       const updatedAnecdote = {...anecdoteToUpdate, votes: anecdoteToUpdate.votes + 1}
@@ -33,6 +16,13 @@ const anecdoteReducer = (state = initialState, action) => {
       return state.map(anecdote => anecdote.id === action.data.id ? updatedAnecdote : anecdote)
     default:
       return state
+  }
+}
+
+export const initialAnecdotes = (data) => {
+  return{
+    type: 'INIT',
+    data,
   }
 }
 

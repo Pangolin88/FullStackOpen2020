22a7770cb5214bd323bb775d3996e58a50fcd72d
Pangolin <huynhviha1703@gmail.com>
2021-02-24 20:22:00 +0700

    part 6.c update note-app

--- practice/frontend/src/App.js
+++ practice/frontend/src/App.js
@@ -9,9 +9,8 @@ import noteService from './services/notes'
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-      noteService.getAll().then(notes => dispatch(initializeNotes(notes)))
-    }
-  , [dispatch])
+    dispatch(initializeNotes())
+  },[dispatch])
 
   return (
     <div>

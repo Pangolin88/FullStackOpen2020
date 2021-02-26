528bfabb20386f8ffb84d40961bc071b15d2e3fc
Pangolin <huynhviha1703@gmail.com>
2021-02-26 15:45:38 +0700

    part 7.a update note-app

--- practice/frontend/src/index.js
+++ practice/frontend/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
 import App from './App'
-import store from './store'
-
+import { BrowserRouter as Router }  from "react-router-dom"
 ReactDOM.render(
-  <Provider store={store}>
+  <Router>
     <App />
-  </Provider>,
+  </Router>,
   document.getElementById('root')
 )
